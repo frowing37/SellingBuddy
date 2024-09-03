2bb67f360d34d7fa013e65562d73c7e384810a2b
frowing37 <froward37@gmail.com>
2024-09-03 22:21:27 +0300

    last

--- src/BuildingBlocks/EventBus/EventBus.AzureServiceBus/obj/Debug/net8.0/EventBus.AzureServiceBus.AssemblyInfo.cs
+++ src/BuildingBlocks/EventBus/EventBus.AzureServiceBus/obj/Debug/net8.0/EventBus.AzureServiceBus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventBus.AzureServiceBus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4098398aae1d122c8807ccee023bef2cb700d8c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afe77943c7e752570b3e86e57f890e450c58db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventBus.AzureServiceBus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventBus.AzureServiceBus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Services/BasketService/BasketService.Api/obj/Debug/net8.0/BasketService.Api.AssemblyInfo.cs
+++ src/Services/BasketService/BasketService.Api/obj/Debug/net8.0/BasketService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasketService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4098398aae1d122c8807ccee023bef2cb700d8c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afe77943c7e752570b3e86e57f890e450c58db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasketService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasketService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
